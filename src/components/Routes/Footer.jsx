d279b6480ade304a3b91f8393673597066c47e82
Paulo Uilian <uilianlbarros@gmail.com>
2022-03-31 14:34:23 -0300

    Atualizando

---
+++ src/components/Routes/Footer.jsx
@@ -0,0 +1,82 @@
+import styled from "styled-components"
+
+import { CircularProgressbar, buildStyles} from "react-circular-progressbar";
+import "react-circular-progressbar/dist/styles.css";
+
+function Footer() {
+
+    const percentage = 80;
+
+    const CircularProgressbarStyle = buildStyles({
+        textColor: '#fff',
+        pathColor: '#fff',
+        trailColor: 'none',
+        textSize: '1.5rem',
+    });
+
+    return (
+        <Conteiner>
+            <div>
+                <p>Hábitos</p>
+                <p>Histórico</p>
+            </div>
+            <div className="circular">
+                <CircularProgressbar
+                    value={percentage}
+                    text={`Hoje`}
+                    strokeWidth={9}
+                    styles={CircularProgressbarStyle}
+                />
+            </div>
+        </Conteiner>
+    )
+}
+
+export default Footer;
+
+const Conteiner = styled.footer`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+
+    position: relative;
+
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 1;
+
+    div {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        height: var(--height-header);
+        padding-inline: 2rem;
+
+        font-size: var(--font-size-p);
+
+        color: var(--color-main);    
+        background-color: #fff;
+    }
+
+    .circular {
+        display: flex;
+        --dimension-circular: 6rem;
+        position: absolute;
+        bottom: 0;
+        left: calc(50% - (var(--dimension-circular) / 2));
+
+        width: var(--dimension-circular);
+        height: var(--dimension-circular);
+
+        border-radius: 50%;
+
+        padding: 6px;
+        margin-bottom: 10px;
+
+        background-color: var(--color-main);
+    }
+`
+
---
+++ src/components/Routes/Teste.jsx
@@ -0,0 +1,138 @@
+import { useState } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+import LogoImg from '../../assets/logo.svg'
+import axios from "axios";
+
+function Teste() {
+    const [userData, setUserData] = useState({
+        email: "",
+        name: "",
+        image: "",
+        password: ""
+    });
+
+    function login(event) {
+        event.preventDefault();
+        
+        const url = 'https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/sign-up';
+
+        const promise = axios.post(url, userData);
+        promise.then(response => {console.log(response.data);});
+        promise.catch(error => {console.log(error);});
+    }
+
+    return (
+        <Conteiner>
+            <Logo src={LogoImg} />
+            <form onSubmit={login} >
+                <input
+                    onChange={e => { setUserData({ ...userData, name: e.target.value }) }}
+                    value={userData.name}
+                    type="name"
+                    placeholder="nome"
+                    required
+                />
+                <input
+                    onChange={e => { setUserData({ ...userData, email: e.target.value }) }}
+                    value={userData.email}
+                    type="email"
+                    placeholder="email"
+                    required
+                />
+                <input
+                    onChange={e => { setUserData({ ...userData, password: e.target.value }) }}
+                    value={userData.password}
+                    type="password"
+                    placeholder="senha"
+                    required
+                />
+                <input
+                    onChange={e => { setUserData({ ...userData, image: e.target.value }) }}
+                    value={userData.image}
+                    type="text"
+                    placeholder="foto"
+                    required>
+
+                </input>
+                <button type="submit">Cadastrar</button>
+            </form>
+            <Link to={'/'}>Já tem uma conta? Faça login!</Link>
+        </Conteiner>
+    );
+}
+
+export default Teste;
+
+const Logo = styled.img`
+    width: 10rem;
+    height: 10rem;
+`
+
+const Conteiner = styled.main`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    width: 100%;
+    height: 100%;
+
+    padding-inline: var(--padding-inline);
+
+    
+    form {
+        display: flex;
+        flex-direction: column;
+        
+        width: 100%;
+        
+        padding-top: 50px;
+        padding-bottom: 25px;
+    }
+
+    form input {
+        height: 3.8rem;
+
+        margin-bottom: 10px;
+        padding-left: 20px;
+
+        font-size: var(--font-size-login);
+        
+        border: 1px solid #D5D5D5;
+        border-radius: 10px;
+    }
+    
+    form input[type="file"] {
+        display: none;
+    }
+
+    form label {
+        height: 3.8rem;
+
+        margin-bottom: 10px;
+
+        font-size: var(--font-size-login);
+        
+        border: 1px solid #D5D5D5;
+        border-radius: 10px;
+
+        color: var(--place-holder);
+    }
+
+    form button {
+        height: 3.5rem;
+
+        font-size: 1.6rem;
+
+        background: var(--color-main);
+        color: #fff;
+        border-radius: 10px;
+    }
+
+    a {
+        font-size: 1.05rem;
+        color: var(--color-main);
+    }
+`
