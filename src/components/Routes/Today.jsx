5d2d37cafaa3b22bde3d4a45533bbfbf2eb22cc5
Paulo Uilian <uilianlbarros@gmail.com>
2022-04-01 09:17:58 -0300

    Exibirá quantidade de tarefas finalizadas

--- src/components/Routes/Today.jsx
+++ src/components/Routes/Today.jsx
@@ -6,8 +6,10 @@ import styled from 'styled-components';
 import Footer from "./Footer";
 import Header from "./Header";
 import axios from 'axios';
+import dayjs from 'dayjs';
 
 function Today() {
+    const [tasksState, setTasksState] = useState({ qtdCompleted: 0, qtdTotal: 0 });
     const [tasks, setTasks] = useState([]);
     const { user } = useContext(UserContext);
     const { url } = useContext(APIUrlContext);
@@ -15,19 +17,44 @@ function Today() {
 
     useEffect(() => {
         const promise = axios.get(`${url}/today`, config);
-        promise.then(response => {
-            setTasks(response.data);
-        });
-        promise.catch(error => { console.log(error.response.data) });
+        promise.then(response => setTasks(response.data));
+        promise.catch(error => console.log(error.response.data));
 
     }, []);
 
-    function setCurrentDay() {
+    useEffect(() => {
+        setTasksState({
+            qtdCompleted: tasks.filter(task => task.done).length,
+            qtdTotal: tasks.length
+        });
+    }, [tasks]);
+
+    function currentDay() {
+        /* require('dayjs/locale/pt-br');
+        dayjs.locale('pt-br');
+        const day = dayjs().format('dddd, DD/MM'); */
+        const daysOfWeek = new Map(
+            [[0, 'Domingo'], [1, 'Segunda-feira'],
+            [2, 'Terça-feira'], [3, 'Quarta-feira'],
+            [4, 'Quinta-feira'], [5, 'Sexta-feira'],
+            [6, 'Sábado']]
+        )
         const dayjs = require('dayjs');
-        const day = dayjs()
-            .locale('pt-br')
-            .format('dddd, DD/MM');
-        return <h2>{day}</h2>;
+        const day = dayjs().format('DD/MM');
+        const weekDay = daysOfWeek.get(dayjs().day());
+
+        return <h2>{weekDay}, {day}</h2>;
+    }
+
+    function currentState() {        
+        const { qtdCompleted, qtdTotal } = tasksState;
+        if(qtdTotal === 0) {
+            return <p>Nenhum hábito concluido ainda</p>
+        } else {
+            return <p className='completed'>
+                {qtdCompleted*100/qtdTotal}% dos hábitos concluídos
+            </p>
+        }
     }
 
     function assembleTasks() {
@@ -41,8 +68,8 @@ function Today() {
         <>
             <Header />
             <Conteiner >
-                {setCurrentDay()}
-                <p>Nenhum hábito concluido ainda</p>
+                {currentDay()}
+                {currentState()}
                 {assembleTasks()}
             </Conteiner>
             <Footer />
