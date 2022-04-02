77ac3b48d0f6a38b4a719b7a8f69acae72cd55c8
Paulo Uilian <uilianlbarros@gmail.com>
2022-04-02 11:07:36 -0300

    Recustomizará botão Hoje do Footer

--- src/components/Routes/Footer.jsx
+++ src/components/Routes/Footer.jsx
@@ -91,12 +91,13 @@ const Conteiner = styled.footer`
         margin-bottom: 10px;
 
         background-color: var(--color-main);
-        border: 1px solid #0f64a1;
-        background-image: linear-gradient(-180deg, #52B6FF 0%, #44a9f1 100%);
-        box-shadow: 0 0.5rem 1.25rem 0 #3489c5,
-                                0 -0.25rem 1.5rem #155d91 inset,
-                                0 0.75rem 0.5rem #738c9e inset,
-                                0 0.25rem 0.5rem 0 #2f56aa inset;
+        border: 1px solid #2270a8;
+        background-image: linear-gradient(-180deg, #52B6FF 0%, #2883c4 100%);
+        box-shadow: 
+                0 0.3rem 1.2rem -5px #000000,
+                0 -0.25rem 1.5rem #2270a8 inset,
+                0 0.75rem 0.5rem #2883c4 inset,
+                0 0.25rem 0.5rem 0 #2883c4 inset;
     }
 
     
