61fd43612c914bb3792927494427449a0dcfcf39
Paulo Uilian <uilianlbarros@gmail.com>
2022-04-03 15:26:41 -0300

    Implementará exibição dos dias que tiveram os hábitos concluidos

--- src/components/Routes/History.jsx
+++ src/components/Routes/History.jsx
@@ -1,28 +1,79 @@
+import CalendarStyle from './CalendarStyled';
+import 'react-calendar/dist/Calendar.css';
+
+import { useEffect, useState, useContext } from 'react';
+
+import APIUrlContext from '../../providers/APIUrlContext';
+import UserContext from '../../providers/UserContext';
 import styled from 'styled-components';
 import Header from './Header';
 import Footer from './Footer';
-
-import '../../css/calendar.css';
-
-import { useState } from 'react'
 import Calendar from 'react-calendar';
+import axios from 'axios';
 
 function History() {
+    const { url } = useContext(APIUrlContext);
+    const { user } = useContext(UserContext);
+    const { config } = user;
+
     const [value, onChange] = useState(new Date());
+    const [dailyHabits, setDailyHabits] = useState(null);
+
+    useEffect(() => {
+        console.log('chamou useEffect');
+        const promise = axios.get(`${url}/history/daily`, config);
+
+        promise.then(response => {
+            console.log(response.data);
+            const habitsMap = new Map(
+                response.data.map(habit => [
+                    habit.day,
+                    habit.habits])
+            )
+            console.log(habitsMap);
+            console.log(typeof (habitsMap));
+            setDailyHabits(habitsMap);
+        });
+        promise.catch(error => console.log(error.response));
+    }, []);
+
+    function dailyHabit(date) {
+        if (dailyHabits) {
+            const dateFormatted = formateDate(date);
+            if (dailyHabits.has(dateFormatted)) {
+                if (habitIsDoned(dailyHabits.get(dateFormatted)))
+                    return <p className='habit habit-doned'>{date.getDate()}</p>
+                else
+                    return <p className='habit habit-not-doned'>{date.getDate()}</p>
+            }
+        }
+        return date.getDate();
+    }
+
+    function habitIsDoned(habits) {
+        return habits.every(habit => habit.done);
+    }
+
+    function formateDate(date) {
+        const dateJoined = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+        return new Date(dateJoined).toLocaleDateString();
+    }
 
     return (
         <>
             <Header />
             <Conteiner>
-                <h2>Histórico</h2>
+                <h2>Histórico </h2>
                 {/* <p>Em breve você poderar ver o histórico dos seus hábitos aqui!</p> */}
-                <div className='calendar'>
+                <CalendarStyle>
                     <Calendar
-                        value={value}
                         onChange={onChange}
-                        view='month'
+                        locale="pt-BR"
+                        value={value}
+                        onClickDay={(e) => { console.log(e.getDate()) }}
+                        formatDay={(locale, date) => dailyHabit(date)}
                     />
-                </div>
+                </CalendarStyle>
             </Conteiner>
             <Footer />
         </>
